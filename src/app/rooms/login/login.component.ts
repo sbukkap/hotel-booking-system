f3ba01ed718b504e556f57865dd74d8ad36c9b14
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2024-06-18 20:41:51 -0400

    Custom Validation using Directives

--- src/app/rooms/login/login.component.ts
+++ src/app/rooms/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HoverDirective } from '../../hover.directive';
+import { EmailvalidatorDirective } from '../../emailValidator/emailvalidator.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, HoverDirective],
+  imports: [FormsModule, HoverDirective, EmailvalidatorDirective, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
